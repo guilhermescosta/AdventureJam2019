f0c739b983bac81bdd24c21fcae8585e5cccd705
Victor B. Dornelles <victor.bogdanowdornelles@gmail.com>
2019-09-07 22:15:57 -0300

    0709

---
+++ Doppelganger/Assets/CabinetScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CabinetScript : MonoBehaviour
+{
+    public bool hasCollided = false;
+    public GameObject MainText;
+    public GameObject cabinText;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    void OnMouseDown()
+    {
+        hasCollided = true;
+
+        MainText = cabinText;
+
+        Debug.Log("Click!");
+    }
+}
