bd7450c56345bd7abfb988d385ddb3462745dfc0
Victor B. Dornelles <victor.bogdanowdornelles@gmail.com>
2019-09-13 22:48:13 -0300

    1309

---
+++ Doppelganger/Assets/ChangeScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class ChangeScene : MonoBehaviour
+{
+    public bool alreadyVisit = false;
+    public Text BackText;
+    public Text GameText;
+    public Button SouthBtn;
+
+    void OnEnable()
+    {
+
+        SouthBtn.onClick.AddListener(MyFunction);
+    }
+
+    void MyFunction()
+    {
+        GameText.text = BackText.text;
+        DontDestroyOnLoad(GameText.gameObject);
+    }
+
+}
